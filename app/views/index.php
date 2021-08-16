7f6d9974c6af685a3ecff0a3a303702f1cfcfb91
Priyanka Dubey <priyankadubey@Priyankas-MacBook-Air.local>
2021-08-16 16:12:35 +0530

    Add heading in index page

--- app/views/index.php
+++ app/views/index.php
@@ -1,6 +1,9 @@
 <?php require_once APPROOT.'/views/includes/header.php';?>
 <body>
 	<div class="container" style="margin-top: 40px">
+		<div class="header" style="padding-bottom: 30px;">
+				<h5 class="text-center">Course Subscription Service</h5>
+		</div>
 		<div style="padding-left:90px;padding-right:90px">
 			<div class="row">
 				<div class="col-sm-6">
